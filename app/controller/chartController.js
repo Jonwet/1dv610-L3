542601ffb10a756f9e567ba8cd5b39870aa4d92e
Jonatan <jw223tb@student.lnu.se>
2025-10-17 16:11:18 +0200

    Add value parsing for the value entry form

--- app/controller/chartController.js
+++ app/controller/chartController.js
@@ -3,6 +3,7 @@ import chartFactory from '../model/chartFactory.js'
 export default class chartController {
     #factory
     #view
+    #currentType
 
     constructor(view) {
         this.#factory = new chartFactory()
@@ -14,15 +15,38 @@ export default class chartController {
     }
 
     connectView() {
-        // this.#view.setOnChartTypeChange((type) => {})
-
+        this.#view.setOnChartTypeChange((type) => {
+            this.#currentType = type
+        })
         this.#view.setOnCreateChart(({ type, title }) => {
             this.#handleCreateChart(type, title)
+        })
+        this.#view.setOnAddData(({ type, label, value }) => {
+            this.#handleAddData(type, label, value)
         })
     }
 
     #handleCreateChart(type, title) {
         const chart = this.#factory.createChart(type, title)
-        // this.#view.renderChart(chart) // Rendering not implemented yet
+        this.#currentType = type
+    }
+
+    #handleAddData(type, label, value) {}
+
+    #parseValues(value) {
+        const parsedValues = String(value)
+            .split(',')
+            .map((v) => v.trim())
+            .filter((v) => v.length > 0)
+            .map((v) => Number(v))
+        return parsedValues
+    }
+
+    #parseValue(value) {
+        const parsedValue = Number(value)
+        if (Number.isNaN(parsedValue)) {
+            throw new Error(`Value must be a number`)
+        }
+        return parsedValue
     }
 }
