caabb59134cd187240bbc6b9e3f28f102737b07c
Jonatan <jw223tb@student.lnu.se>
2025-11-18 19:18:23 +0100

    Add methods for combatant states

--- models/CombatModel.js
+++ models/CombatModel.js
@@ -1,6 +1,5 @@
-import CombatSystem from '../modules/CombatSystem.js'
-import Combatant from '../modules/Combatant.js'
-import CombatAction from '../modules/CombatAction.js'
+import CombatSystem from '../module/CombatSystem.js'
+import Combatant from '../module/Combatant.js'
 
 export default class CombatModel {
     constructor() {
@@ -37,4 +36,83 @@ export default class CombatModel {
     checkBattleEnd() {
         return this.combat.checkBattleEnd()
     }
+
+    getCurrentCombatant() {
+        return this.combat.getState().currentCombatant
+    }
+
+    getAllCombatants() {
+        return this.combat.getState().combatants
+    }
+
+    getPlayerTeam() {
+        return this.getAllCombatants().filter((c) => c.team === 'Player')
+    }
+
+    getEnemyTeam() {
+        return this.getAllCombatants().filter((c) => c.team === 'Enemy')
+    }
+
+    getAliveEnemies() {
+        return this.getEnemyTeam().filter((c) => c.isAlive())
+    }
+
+    getAlivePlayers() {
+        return this.getPlayerTeam().filter((c) => c.isAlive())
+    }
+
+    getCombatLog() {
+        return this.combat.getCombatLog()
+    }
+
+    getWinner() {
+        return this.combat.getWinner()
+    }
+
+    isPlayerTurn() {
+        const current = this.getCurrentCombatant()
+        return current && current.team === 'Player'
+    }
+
+    getCombatantById(id) {
+        return this.getAllCombatants().find((c) => c.id === id)
+    }
+
+    isInitialized() {
+        return this.combat !== null
+    }
+
+    isActive() {
+        if (!this.combat) return false
+        return this.combat.getState().isActive
+    }
 }
+
+// Test
+const playerUnits = [
+    {
+        id: 1,
+        name: 'Hero',
+        team: 'Player',
+        maxHealth: 100,
+        attackPower: 20,
+        defense: 10,
+        speed: 15,
+    },
+]
+
+const enemyUnits = [
+    {
+        id: 2,
+        name: 'Goblin',
+        team: 'Enemy',
+        maxHealth: 80,
+        attackPower: 15,
+        defense: 5,
+        speed: 10,
+    },
+]
+
+const combat = new CombatModel()
+combat.initializeCombat(playerUnits, enemyUnits)
+console.log(combat.getCurrentCombatant())
