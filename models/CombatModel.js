9d14390aa54fdb976c4f97928dfe655cc6fffd53
Jonatan <jw223tb@student.lnu.se>
2025-11-18 16:48:46 +0100

    Add combat state methods

--- models/CombatModel.js
+++ models/CombatModel.js
@@ -1,5 +1,6 @@
 import CombatSystem from '../modules/CombatSystem.js'
 import Combatant from '../modules/Combatant.js'
+import CombatAction from '../modules/CombatAction.js'
 
 export default class CombatModel {
     constructor() {
@@ -13,5 +14,27 @@ export default class CombatModel {
 
         this.playerCombatants = playerUnits.map((unit) => new Combatant(unit))
         this.enemyCombatants = enemyUnits.map((unit) => new Combatant(unit))
+
+        const allCombatants = [
+            ...this.playerCombatants,
+            ...this.enemyCombatants,
+        ]
+        this.combat.startCombat(allCombatants)
+    }
+
+    executeAttack(targetId, action) {
+        this.combat.executeAttack(targetId, action)
+    }
+
+    executeDefend(unitId) {
+        this.combat.executeDefend(unitId)
+    }
+
+    advanceTurn() {
+        this.combat.nextUnitTurn()
+    }
+
+    checkBattleEnd() {
+        return this.combat.checkBattleEnd()
     }
 }
