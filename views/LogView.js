1be826bfcc7cc5d6084e7534f8e3da840efdd691
Jonatan <jw223tb@student.lnu.se>
2025-11-21 13:13:04 +0100

    Add combat log view

---
+++ views/LogView.js
@@ -0,0 +1,27 @@
+export default class LogView {
+    constructor() {
+        this.logContainer = document.getElementById('combat-log')
+    }
+
+    addLogMessage(message) {
+        const logEntry = document.createElement('div')
+        logEntry.className = 'log-entry'
+        logEntry.textContent = message
+
+        this.logContainer.appendChild(logEntry)
+
+        this.scrollToBottom()
+    }
+
+    clearLog() {
+        this.logContainer.innerHTML = ''
+    }
+
+    scrollToBottom() {
+        this.logContainer.scrollTop = this.logContainer.scrollHeight
+    }
+
+    addLogMessages(messages) {
+        messages.forEach((message) => this.addLogMessage(message))
+    }
+}
