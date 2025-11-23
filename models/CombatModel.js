65e556d017658930164d658f9d187b2a43f8b4af
Jonatan <jw223tb@student.lnu.se>
2025-11-23 15:56:37 +0100

    Make methods private

--- models/CombatModel.js
+++ models/CombatModel.js
@@ -2,52 +2,55 @@ import CombatSystem from '../module/CombatSystem.js'
 import Combatant from '../module/Combatant.js'
 
 export default class CombatModel {
+    #combatSystem
+    #player
+    #enemy
     constructor() {
-        this.combatSystem = null
-        this.player = null
-        this.enemy = null
+        this.#combatSystem = null
+        this.#player = null
+        this.#enemy = null
     }
 
     initializeCombat(playerUnit, enemyUnit) {
-        this.combatSystem = new CombatSystem()
+        this.#combatSystem = new CombatSystem()
 
-        this.player = new Combatant(playerUnit)
-        this.enemy = new Combatant(enemyUnit)
+        this.#player = new Combatant(playerUnit)
+        this.#enemy = new Combatant(enemyUnit)
 
-        const allCombatants = [this.player, this.enemy]
-        this.combatSystem.startCombat(allCombatants)
+        const allCombatants = [this.#player, this.#enemy]
+        this.#combatSystem.startCombat(allCombatants)
     }
 
     executeAttack(targetId, action) {
-        return this.combatSystem.executeAttack(targetId, action)
+        return this.#combatSystem.executeAttack(targetId, action)
     }
 
     executeDefend(unitId) {
-        return this.combatSystem.executeDefend(unitId)
+        return this.#combatSystem.executeDefend(unitId)
     }
 
     advanceTurn() {
-        return this.combatSystem.nextUnitTurn()
+        return this.#combatSystem.nextUnitTurn()
     }
 
     checkBattleEnd() {
-        return this.combatSystem.checkBattleEnd()
+        return this.#combatSystem.checkBattleEnd()
     }
 
     getCurrentCombatant() {
-        return this.combatSystem.getState().currentCombatant
+        return this.#combatSystem.getState().currentCombatant
     }
 
     getPlayer() {
-        return this.player
+        return this.#player
     }
 
     getEnemy() {
-        return this.enemy
+        return this.#enemy
     }
 
     getWinner() {
-        return this.combatSystem.getWinner()
+        return this.#combatSystem.getWinner()
     }
 
     isPlayerTurn() {
